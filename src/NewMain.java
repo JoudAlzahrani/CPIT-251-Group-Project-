cd2e6e24003b8dbe3ab3e63e92559c3a83a4b02e
Lina-M3 <linamohammed.2003.z@gmail.com>
2024-11-30 22:29:34 +0300

    first commit

--- src/NewMain.java
+++ src/NewMain.java
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-// testing of pull 
-// lina 
-// يارب يزبط
-//الحمدلله 
-//joud_test_ يارببب
-// استغفرالله
-// لا اله الا الله
-// تجربة لينا 
 
 import java.io.IOException;
 import java.nio.file.Files;
