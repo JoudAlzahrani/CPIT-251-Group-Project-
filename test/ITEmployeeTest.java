37fedd60b40440d8b12e2a1651a1dfe9d432f4d9
Joud-alharbi-1 <Joudh2002.h@gmail.com>
2024-12-05 00:07:03 +0300

    testing for ITEmployee Function_Part2

--- test/ITEmployeeTest.java
+++ test/ITEmployeeTest.java
@@ -1,11 +1,12 @@
+
 import org.junit.Before;
 import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Scanner;
 import static org.junit.Assert.*;
 
-
 public class ITEmployeeTest {
+
     private ITEmployee itEmployee;
     private ArrayList<FeedBack> feedbackList;
     private FileHandler fileHandler;
@@ -21,9 +22,9 @@ public class ITEmployeeTest {
     public void RespondToUnsolvedIssue_StatusUpdatedTest() {
         feedbackList.add(new FeedBack("FB1", "Printer issue", "Test", "001", "Ali Ahmed"));
         Scanner scanner = new Scanner("1\nReplaced toner\n1\n");
-        
+
         itEmployee.viewAndRespondToUnsolvedIssues(feedbackList, scanner, fileHandler);
-        
+
         assertEquals("Solved", feedbackList.get(0).getStatus());
     }
 
@@ -31,13 +32,17 @@ public class ITEmployeeTest {
     public void RespondToUnsolvedIssue_ITResponseTest() {
         feedbackList.add(new FeedBack("FB1", "Printer issue", "Test", "001", "Ali Ahmed"));
         Scanner scanner = new Scanner("1\nReplaced toner\n1\n");
-        
+
         itEmployee.viewAndRespondToUnsolvedIssues(feedbackList, scanner, fileHandler);
 
         assertEquals("Replaced toner", feedbackList.get(0).getItResponse());
     }
-     
 
-    
+    @Test
+    public void testFeedbackSetItResponse() {
+        FeedBack feedback = new FeedBack("FB1", "Printer issue", "The printer is not working", "001", "Ali Ahmed");
+        feedback.setItResponse("Fixed");
+        assertEquals("Fixed", feedback.getItResponse());
+    }
+
 }
-
